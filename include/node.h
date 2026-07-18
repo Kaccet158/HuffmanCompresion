ed52ee9161c89d7abb2b9dcc64e587d5c6768616
Kacper <kacperceteraonett@gmail.com>
2026-07-18 14:08:48 +0200

    Add InternalNode class

--- include/node.h
+++ include/node.h
@@ -4,9 +4,10 @@
 #include <iostream>
 #include <memory>
 
+// Klasa wezla w drzewie
 class Node {
     protected:
-        int frequency;
+        int frequency; // Czestotliwosc
     public:
         Wezel(int fr); // Konstruktor
         virtual ~Wezel() = default; // Destruktor
@@ -14,15 +15,27 @@ class Node {
         virtual bool isLeaf() const = 0; // Metoda na razie wirtualna
 };
 
+// Klasa Liscia w Wezle
 class LeafNode : public Node {
     private:
-        char character;
+        char character; // Znak w drzewie
     public;
         LeafNode(int ch, int fr); // Konstruktor ile wystapien dla danego znaku
         bool isLeaf() const override; // Musimy nadpisac, bo juz nasz rodzic ma klase abrkacyjne
         char getChar() const; // Metoda dostania znaku
         friend std::ostream& operator<<(std::ostream& os, const LeafNode& leaf); // Przyjaciel i nadanie std::cout skrotu os
+};
 
+// Klasa poloczenai dwoch wezlow wewnetrznych
+class InternalNode : public Node {
+    private:
+        std::shared_ptr<Node> left; // Pointer na lewe dziecko
+        std::shared_ptr<Node> right; // Pointer na prawe
+    public:
+        InternalNode(std::shared_ptr<Node> l, std::shared_ptr<Node> r) // Konstruktor, ktory bedzie wskazywac nam na te dzieci
+        bool isLeaf() cosnt override; // Tez musi sprawdzic czy lisciem
+        std::shared_ptr<Node> getLeft() const; // Dostanie lewe dziecko
+        std::shared_ptr<Node> getRight() const; // Dostanei prawe dziecko
 };
 
 #endif
